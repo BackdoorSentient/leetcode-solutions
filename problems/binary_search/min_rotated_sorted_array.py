ad55b5cd9fb86a682417c25d810a89f696c14406
BackdoorSentient <aniketwaichal60@gmail.com>
2026-05-01 19:08:04 +0530

    added new solution

--- problems/binary_search/min_rotated_sorted_array.py
+++ problems/binary_search/min_rotated_sorted_array.py
@@ -58,3 +58,14 @@ class Solution:
 
 sol = Solution()
 print(sol.findMin([3, 4, 5, 1, 2]))
+
+
+from typing import List
+
+class MyFirstSolution:
+    def findMin(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[0]
+    
+# sort solution = O(n log n) above is this 
+# binary search solution = O(log n) but this is expected
